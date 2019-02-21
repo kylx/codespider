ae5bc24f671a1a0d3e4372084c5dda22fe22a96e
kylx <kbdelcampo@up.edu.ph>
2019-02-21 19:57:40 +0800

    Add mysql as database

--- codespider/settings.py
+++ codespider/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'codespider.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# Read more: https://docs.djangoproject.com/en/2.1/ref/databases/
+# settings.py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'codespider/db.cnf',
+            'sql_mode': 'STRICT_TRANS_TABLES',
+        },
     }
 }
 
