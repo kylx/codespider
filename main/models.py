be2fc162a1e217927de89795f1b5d0cfabe0616c
Wednesday2009 <jpsamonte@up.edu.ph>
2019-03-20 16:08:23 +0800

    Add Patient Table w/out the Region, City and Province

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    age = models.SmallIntegerField()
+    sex = models.CharField(max_length=1)
+    
+
