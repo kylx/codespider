dc97b1f35237b62b0c5aa6fd93cfb958ce44b72f
unknown <kbdelcampo@up.edu.ph>
2019-04-09 16:56:29 +0800

    implemented location filtering logic

--- main/startup.py
+++ main/startup.py
@@ -1,12 +1,19 @@
+from .enums import Enums
+
+def filter_location(location_list, location_filter):
+    return [location for location in location_list if location[0].startswith(location_filter[0])]
 
 def run():
-	print('Running startup...')
-	
-	
-	# read csv/json
-	# Initialize enums
-	# blah blah
-	
-	# Kung gusto mag output for debugging/testing, pwede pud diri
-	# import lg mga files/modules then do stuff
-	
+    print('Running startup...')
+    print(Enums.REGIONS)
+    print(Enums.PROVINCES)
+    
+    # r = Enums.REGIONS[0]
+    # fp = filter(Enums.PROVINCES
+    
+    for region in Enums.REGIONS:
+        print(region)
+        for province in filter_location(Enums.PROVINCES, region):
+            print('   ' + ' '.join(province))
+            for city in filter_location(Enums.CITIES, province):
+                print('       ' + ' '.join(city))
