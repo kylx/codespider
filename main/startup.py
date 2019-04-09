b1035a534863766c091ccc1aa79a3e4a3522b504
unknown <kbdelcampo@up.edu.ph>
2019-04-09 19:43:41 +0800

    filtering in template. Works up until region8, leyte

--- main/startup.py
+++ main/startup.py
@@ -1,19 +1,22 @@
 from .enums import Enums
+# from .forms import PatientForms
 
 def filter_location(location_list, location_filter):
     return [location for location in location_list if location[0].startswith(location_filter[0])]
 
 def run():
-    print('Running startup...')
-    print(Enums.REGIONS)
-    print(Enums.PROVINCES)
+	pass
+	# print(PatientForms())
+    # print('Running startup...')
+    # print(Enums.REGIONS)
+    # print(Enums.PROVINCES)
     
     # r = Enums.REGIONS[0]
     # fp = filter(Enums.PROVINCES
     
-    for region in Enums.REGIONS:
-        print(region)
-        for province in filter_location(Enums.PROVINCES, region):
-            print('   ' + ' '.join(province))
-            for city in filter_location(Enums.CITIES, province):
-                print('       ' + ' '.join(city))
+    # for region in Enums.REGIONS:
+        # print(region)
+        # for province in filter_location(Enums.PROVINCES, region):
+            # print('   ' + ' '.join(province))
+            # for city in filter_location(Enums.CITIES, province):
+                # print('       ' + ' '.join(city))
