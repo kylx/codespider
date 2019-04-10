a1368bc048a6ec0bd63c8d7cde8e3187e3f7e566
Wednesday2009 <jpsamonte@up.edu.ph>
2019-04-10 10:14:18 +0800

    Show LastName of Patient instead of "Object" in Django

--- main/models.py
+++ main/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
 class Patient(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    middle_initial = models.CharField(max_length=1)
-    age = models.SmallIntegerField()
-    sex = models.CharField(max_length=1)
-    diagnosis = models.ForeignKey("Diagnosis",on_delete=models.CASCADE,null=True,blank=True)
-    region = models.CharField(max_length=6)
-    province = models.CharField(max_length=6)
-    city = models.CharField(max_length=6, null=True, blank=True)
- 
+	first_name = models.CharField(max_length=20)
+	last_name = models.CharField(max_length=20)
+	middle_initial = models.CharField(max_length=1)
+	age = models.SmallIntegerField()
+	sex = models.CharField(max_length=1)
+	diagnosis = models.ForeignKey("Diagnosis",on_delete=models.CASCADE,null=True,blank=True)
+	region = models.CharField(max_length=6)
+	province = models.CharField(max_length=6)
+	city = models.CharField(max_length=6, null=True, blank=True)
+	def __str__(self):
+		return self.last_name
+
 class Diagnosis(models.Model):
 	full_name = models.CharField(max_length=60)
 	short_name = models.CharField(max_length=30, blank=True)
