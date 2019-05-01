5c93d3ebb337031644d97ea7814ee85e760fa197
Allein Lyle <ironlyle@gmail.com>
2019-05-01 15:41:01 +0800

    Increment date by clicking save

--- main/static/js/x/date.js
+++ main/static/js/x/date.js
@@ -1,8 +1,27 @@
-var isDay = { day: 'numeric' };
+var isDay = { day: 'numeric' } ;
 var isDayWeek = { weekday: 'long' };
 var isMonthYear = { month: 'long', year: 'numeric' };
 
-var dt = new Date();
-document.getElementById("theDay").innerHTML = dt.toLocaleDateString("en-US", isDay);
-document.getElementById("theDayWeek").innerHTML = dt.toLocaleDateString("en-US", isDayWeek);
-document.getElementById("theMonthYear").innerHTML = dt.toLocaleDateString("en-US", isMonthYear);
+var datetest = new Date();
+document.getElementById("theDay").innerHTML = datetest.toLocaleDateString("en-US", isDay);
+document.getElementById("theDayWeek").innerHTML = datetest.toLocaleDateString("en-US", isDayWeek);
+document.getElementById("theMonthYear").innerHTML = datetest.toLocaleDateString("en-US", isMonthYear);
+
+
+$("#datetest").text(datetest.toDateString());
+
+$('#decr').on('click', function() {
+  datetest = new Date(Date.parse($('#datetest').text()));
+  datetest.setDate(datetest.getDate() - 1);
+  $('#datetest').text(datetest.toDateString());
+});
+
+$('#incr').on('click', function() {
+    document.getElementById("theDay").innerHTML = datetest.setDate(datetest.getDate() + 1);
+    document.getElementById("theDay").innerHTML = datetest.toLocaleDateString("en-US", isDay) ;
+    document.getElementById("theDayWeek").innerHTML = datetest.toLocaleDateString("en-US", isDayWeek);
+    document.getElementById("theMonthYear").innerHTML = datetest.toLocaleDateString("en-US", isMonthYear);
+  // datetest = new Date(Date.parse($('#datetest').text()));
+  // datetest.setDate(datetest.getDate() + 1);
+  // $('#datetest').text(datetest.toDateString());
+});
