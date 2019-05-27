5246ec11420a273abc821f0caa80e6b4685430eb
paredesgeomarie <paredesgeomarie@gmail.com>
2019-05-27 17:30:02 +0800

    edited inquiry page

--- main/forms.py
+++ main/forms.py
@@ -153,18 +153,25 @@ class FilterForm(ModelForm):
         model = Patient
         fields = ['last_name', 'first_name', 'middle_initial']
 		
-    date_from = forms.DateField(
-	widget=forms.DateInput( format = '%m/%d/%Y' ),
-	input_formats = ( '%m/%d/%Y' )
-    )
+    date_from = forms.DateField (
+        widget = forms.DateInput (
+			format = '%m/%d/%Y',
+			attrs = {
+				'type': 'date',
+				'class': 'form-control'
+			})
+		)
     
     date_to = forms.DateField(
-        widget=forms.DateInput( format = '%m/%d/%Y' ),
-        input_formats = ( '%m/%d/%Y' )
-    )
+        widget = forms.DateInput (
+			format = '%m/%d/%Y',
+			attrs = {
+				'type': 'date',
+				'class': 'form-control'
+			})
+		)
 	
     patient = forms.MultipleChoiceField (
-        label = 'Relationship',
 		widget = forms.CheckboxSelectMultiple,
 		choices = [
 			['p-a', 'All Patients'],
@@ -174,7 +181,6 @@ class FilterForm(ModelForm):
     )
 	
     watcher = forms.MultipleChoiceField (
-        label = 'Relationship',
 		widget = forms.CheckboxSelectMultiple,
 		choices = [
 			['w', 'Watchers'],
@@ -182,7 +188,6 @@ class FilterForm(ModelForm):
     )
 	
     building = forms.MultipleChoiceField (
-        label = 'Relationship',
 		widget = forms.CheckboxSelectMultiple,
 		choices = [
 			['b-al', 'All Buildings'],
@@ -191,62 +196,35 @@ class FilterForm(ModelForm):
 		]
     )
 	
-    diagnosis = forms.CharField (
-        label = 'Diagnosis',
-        widget = forms.Select (
-			attrs = {
-				'class' : 'form-control, input'
-			},
-		    choices = [
-				['default', 'Diagnosis'],
-			    ['all', 'Acute Lymphoblastic Leukemia'],
-		        ['bg', 'Brainstem Glioma'],
-	        ]
-            # choices = DIAGNOSIS
-        )
-    )
-    
-    region = forms.CharField (
-        label = 'Region',
-        widget = forms.Select (
-			attrs = {
-				'class' : 'form-control, input'
-			},
-            choices = [
-				['default', 'Region'],
-			    ['r1', 'Region I (Ilocos Region)'],
-		        ['r2', 'Region II (Cagayan Valley)'],
-	        ]
-			# choices = Enums.REGIONS
-        )
-    )
-    
-    province = forms.CharField (
-        label = 'Province',
-        widget = forms.Select (
-			attrs = {
-				'class' : 'form-control, input'
-			},
-		    choices = [
-				['default', 'Province'],
-			    ['r1-1', 'Ilocos Norte'],
-		        ['r1-2', 'Ilocos Sur'],
-	        ]
-            # choices = Enums.PROVINCES
-        )
-    )
-    
-    city = forms.CharField (
-        label = 'City',
-        widget = forms.Select (
-			attrs = {
-				'class' : 'form-control, input'
-			},
-		    choices = [
-				['default', 'City'],
-			    ['r1-1-1', 'Batac'],
-		        ['r1-1-2', 'Laoag'],
-	        ]
-            # choices = Enums.CITIES
-        )
-    )	
+    class Meta:
+        model = Patient
+        fields = ['diagnosis', 'region', 'province', 'city']
+        widgets = {
+            'diagnosis': forms.Select (
+			    attrs = {
+				    'class' : 'form-control, input'
+			    },
+                choices = get_diagnosis_list(),
+            ),
+			
+			'region': forms.Select (
+			    attrs = {
+				    'class' : 'form-control, input',
+			    },
+                choices = Enums.REGIONS,
+            ),
+			
+			'province': forms.Select (
+			    attrs = {
+				    'class' : 'form-control, input'
+			    },
+                choices = Enums.PROVINCES
+            ),
+			
+			'city': forms.Select (
+			    attrs = {
+				    'class' : 'form-control, input'
+			    },
+                choices = Enums.CITIES
+            )
+		}
